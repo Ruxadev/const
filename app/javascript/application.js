e40ee657ff2b8b12a0a586d5e0aeafbb62e17dc7
Ruxadev <grmdev01@gmail.com>
2024-08-19 21:01:14 -0300

    add views/project/new and initial javascript fixing

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,31 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
+
+
+document.addEventListener("turbo:load", function() {
+    let elements = document.querySelectorAll("[data-method='delete']");
+    elements.forEach(function(element) {
+      element.addEventListener("click", function(e) {
+        e.preventDefault();
+        let message = element.getAttribute("data-confirm");
+        if (message === null || confirm(message)) {
+          let form = document.createElement("form");
+          form.method = "POST";
+          form.action = element.getAttribute("href");
+          let input = document.createElement("input");
+          input.type = "hidden";
+          input.name = "_method";
+          input.value = "delete";
+          form.appendChild(input);
+          let tokenInput = document.createElement("input");
+          tokenInput.type = "hidden";
+          tokenInput.name = "authenticity_token";
+          tokenInput.value = document.querySelector('meta[name="csrf-token"]').content;
+          form.appendChild(tokenInput);
+          document.body.appendChild(form);
+          form.submit();
+        }
+      });
+    });
+  });
